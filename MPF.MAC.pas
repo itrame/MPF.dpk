099464baaf125e73698f7ec4668d9a7b81c7db87
itrame <marcinpfeiffer@gmail.com>
2021-02-24 22:58:07 +0100

    Dodano klasę i interfejs IMACAddress w MPF.MAC.

---
+++ MPF.MAC.pas
@@ -0,0 +1,66 @@
+unit MPF.MAC;
+interface uses SysUtils;
+
+//==============================================================================
+type
+  IMACAddress = interface['{B34C6408-3E3A-4611-A17C-EE2FF5C4A47F}']
+    function AsString(const ASeparator: string = ''): string;
+    function GetByte(Index: Integer): Byte;
+    property Bytes[Index: Integer]: Byte read GetByte;
+  end;
+
+//==============================================================================
+function NewMACAddress(const AData: TBytes): IMACAddress;
+
+//==============================================================================
+implementation
+
+//==============================================================================
+type
+  TMACAddress = class(TInterfacedObject, IMACAddress)
+  strict private
+    Bytes: TBytes;
+    function AsString(const ASeparator: string = ''): string;
+    function GetByte(Index: Integer): Byte;
+  public
+    constructor Create(const AData: TBytes);
+  end;
+
+//==============================================================================
+{ TMACAddress }
+
+function TMACAddress.AsString(const ASeparator: string = ''): string;
+var
+  i: Integer;
+begin
+  Result := '';
+  for i:=0 to 5 do begin
+    Result := IntToHex(Bytes[i], 2);
+    if i<5 then Result := Result + ASeparator;
+  end;
+end;
+
+//------------------------------------------------------------------------------
+constructor TMACAddress.Create(const AData: TBytes);
+begin
+  inherited Create;
+  Bytes := AData;
+end;
+
+//------------------------------------------------------------------------------
+function TMACAddress.GetByte(Index: Integer): Byte;
+begin
+  Result := Bytes[Index];
+end;
+
+//==============================================================================
+function NewMACAddress(const AData: TBytes): IMACAddress;
+begin
+  if Length(AData) <> 6 then
+    raise Exception.Create('Wrong data size. MAC Address should have 6 bytes.');
+
+  Result := TMACAddress.Create(AData);
+
+end;
+
+end.
